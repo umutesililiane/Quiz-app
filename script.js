1ba2c0c8a08aa1101e5a46f1840792e8df1b54f8
umutesililiane <umutesililiane4@gmail.com>
2023-08-07 03:23:49 +0530

    display correct answer directly when incorrect answer is selected & display next button

--- script.js
+++ script.js
@@ -84,6 +84,12 @@ function selectAnswer(e){
     }else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
 
