4efdd497d5e821317802eef8514bc9c95c982397
umutesililiane <umutesililiane4@gmail.com>
2023-08-07 03:51:09 +0530

    add scores functionality

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
@@ -92,5 +93,28 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+         handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
 
 startQuiz();
